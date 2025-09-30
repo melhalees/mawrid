b49932e5b22a8d0022af8a465ec65673c1c89739
Mohamed Elhalees <melhalees@outlook.com>
2025-09-30 23:05:47 +0300

    Initial empty spring boot project

---
+++ src/main/java/sa/mawrid/erp/MawridApplication.java
@@ -0,0 +1,13 @@
+package sa.mawrid.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MawridApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MawridApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sa/mawrid/erp/MawridApplicationTests.java
@@ -0,0 +1,13 @@
+package sa.mawrid.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MawridApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
